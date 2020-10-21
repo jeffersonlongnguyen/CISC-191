3544a9891d84ca3a4efd5ef8885d98c5ebbaeaf6
Jefferson Nguyen <jeffersonlongnguyen@gmail.com>
2020-10-21 00:33:01 -0700

    CourseGradePrinter Completed.  Added info.

--- Week1/src/com/jeffersonnguyen/chapter5/CourseGradePrinter.java
+++ Week1/src/com/jeffersonnguyen/chapter5/CourseGradePrinter.java
@@ -1,4 +1,17 @@
 package com.jeffersonnguyen.chapter5;
+/**
+ * Challenge Activity 5.2.3
+ * Write a for loop to print all elements in courseGrades, following each element with a space (including the last). Print forwards, then backwards. End each loop with a newline. Ex: If courseGrades = {7, 9, 11, 10}, print:
+ * 7 9 11 10
+ * 10 11 9 7
+ *
+ * Hint: Use two for loops. Second loop starts with i = courseGrades.length - 1. (Notes)
+ *
+ *
+ * Note: These activities may test code with different test values. This activity will perform two tests, both with a 4-element array. See "How to Use zyBooks".
+ *
+ * Also note: If the submitted code tries to access an invalid array element, such as courseGrades[9] for a 4-element array, the test may generate strange results. Or the test may crash and report "Program end never reached", in which case the system doesn't print the test case that caused the reported message.
+ */
 
 import java.util.Scanner;
 
