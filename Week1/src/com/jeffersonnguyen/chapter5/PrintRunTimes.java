ee640f08fb22ebfa04c2e937b33105dee7ee44a4
Jefferson Nguyen <jeffersonlongnguyen@gmail.com>
2020-10-21 00:21:59 -0700

    CourseGradePrinter Completed

---
+++ Week1/src/com/jeffersonnguyen/chapter5/PrintRunTimes.java
@@ -0,0 +1,21 @@
+package com.jeffersonnguyen.chapter5;
+
+import java.util.Scanner;
+
+public class PrintRunTimes {
+    public static void main (String [] args) {
+        Scanner scnr = new Scanner(System.in);
+        final int NUM_ELEMENTS = 5;
+        int [] runTimes = new int[NUM_ELEMENTS];
+        int i;
+
+        for (i = 0; i < runTimes.length; ++i) {
+            runTimes[i] = scnr.nextInt();
+        }
+
+        /* Your solution goes here  */
+        System.out.println(runTimes[0]);
+        System.out.println(runTimes[1]);
+        System.out.println(runTimes[2]);
+    }
+}
