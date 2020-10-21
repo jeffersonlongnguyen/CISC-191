dcae6454d13de3c3a0d22949cc47e2bd4dcb2f3e
Jefferson Nguyen <jeffersonlongnguyen@gmail.com>
2020-10-21 00:35:50 -0700

    CourseGradePrinter Completed.  Added info.

--- Week1/src/com/jeffersonnguyen/chapter5/PrintRunTimes.java
+++ Week1/src/com/jeffersonnguyen/chapter5/PrintRunTimes.java
@@ -1,4 +1,17 @@
 package com.jeffersonnguyen.chapter5;
+/**
+ * Prompt and source code copied from zyBooks
+ * Challenge Activity 5.2.2
+ * Write three statements to print the first three elements of array runTimes. Follow each statement with a newline. Ex: If runTime = {800, 775, 790, 805, 808}, print:
+ * 800
+ * 775
+ * 790
+ *
+ *
+ * Note: These activities will test the code with different test values. This activity will perform two tests, both with a 5-element array. See "How to Use zyBooks".
+ *
+ * Also note: If the submitted code tries to access an invalid array element, such as runTimes[9] for a 5-element array, the test may generate strange results. Or the test may crash and report "Program end never reached", in which case the system doesn't print the test case that caused the reported message.
+ */
 
 import java.util.Scanner;
 
